5e04741a76d5c94268f5a1ba1714de4e74ee2b4b
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-03-07 19:21:10 -0500

    Chapter20 web services

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Erik Uecke on 3/7/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchInterestingPhotos {
+            (photosResult) -> Void in
+            
+            switch photosResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count).")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+        }
+    }
+    
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
+    }
+}
